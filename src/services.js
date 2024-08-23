bc2a0b34fb23e6604474295746e0b953a39a73c1
DarthVada36 <velazquez.vada@outlook.com>
2024-08-23 15:13:08 +0200

    Add: GET services and print characters

---
+++ src/services.js
@@ -0,0 +1,49 @@
+const URL_API = "http://localhost:3000/characters"
+
+//CREATE Method: POST
+async function createCharacter() {
+    
+}
+
+
+//READ Method: GET
+
+async function getAllCharacters() {
+    const response = await fetch (URL_API, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",       
+        },
+    })
+    const characters = await response.json () //pasar datos a formato json
+    return characters
+}
+
+const listTag = document.getElementById ('charactersList')
+
+async function printCharacter() {
+    const characters = await getAllCharacters()
+    characters.map((character) => {
+        listTag.innerHTML += `<li>
+        <p>${character.id}</p>
+        <p>${character.name}</p>
+        <p>${character.house}</p>
+        <p>${character.age}</p>
+    </li>`
+    })
+}
+
+printCharacter()
+
+//UPDATE Method: EDIT
+async function updateCharacter() {
+    
+}
+
+//DELETE Method: DELETE
+
+async function deleteCharacter() {
+    
+}
+
+//RENDER Function
